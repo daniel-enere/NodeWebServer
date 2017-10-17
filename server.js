40d9c23c0558985b0cb70b4cb544ae3063a9a9af
daniel-enere <daniel.enere@gmail.com>
2017-10-17 14:16:15 -0500

    added css features

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const $ = require('jquery');
+
 
 const port = process.env.PORT || 3000;
 var app = express();
